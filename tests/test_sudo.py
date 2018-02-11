aca8b8d7ed0b0dacd7cb7186ecfb97609d7ee3f4
Dekel Yigal <dekelyi@gmail.com>
2018-02-11 10:45:31 +0200

    fix linting

--- tests/test_sudo.py
+++ tests/test_sudo.py
@@ -1,8 +1,13 @@
-import os
+"""
+Test to the sudo premissions
+"""
 import subprocess
 
 
 def test_passwd_premission():
+    """
+    Test that the program has premissions to edit the PASSWD file
+    """
     try:
         subprocess.call(['sudo', 'test', '-w', '/etc/passwd'])
     except Exception as err:
