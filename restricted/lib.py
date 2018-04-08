6d843144fac5407607b465839032806eba512821
Dekel Yigal <dekelyi@gmail.com>
2018-04-08 13:53:36 +0300

    done

--- restricted/lib.py
+++ restricted/lib.py
@@ -2,8 +2,10 @@
 Main library file
 """
 import logging
+import subprocess
 from . import utils
 from . import consts
+from . import errors
 
 logging.basicConfig(format='%(levelname)s [%(asctime)s]: %(message)s', level=logging.DEBUG)
 
@@ -20,6 +22,22 @@ class User(object):
         self.user = user or utils.random_str(10)
         self.group = group
 
+        if not utils.is_group_exists(group):
+            utils.call_wrapper(['groupadd', group])
+
+        ret = utils.call_wrapper(['useradd', self.user, '-G', group])
+        
+        if ret == 9:
+            raise errors.UserExistsError
+        elif ret == 1:
+            raise errors.PremissionError
+
         logging.info('Created user %s of group %s', self.user, self.group)
+        self._executed = True
+
+    def __del__(self):
+        if not getattr(self, '_executed', False):
+            return
+        utils.call_wrapper(['userdel', self.user])
 
 __all__ = ['User']
