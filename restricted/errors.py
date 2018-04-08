36498da22481641f5809ece6e54671ad680dd901
Dekel Yigal <dekelyi@gmail.com>
2018-04-08 13:57:02 +0300

    dlcstrings

--- restricted/errors.py
+++ restricted/errors.py
@@ -13,4 +13,4 @@ class PremissionError(Exception):
     The process does not have the premissions to manage users and groups
     """
 
-__all__ = ['UserExistsError']
+__all__ = ['UserExistsError', 'PremissionError']
