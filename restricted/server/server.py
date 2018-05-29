2c6a28c8b3e0cb1249e8e483ba1d3851d473d2ae
Dekel Yigal <dekelyi@gmail.com>
2018-05-29 22:34:54 +0300

    Removed `buffer`

--- restricted/server/server.py
+++ restricted/server/server.py
@@ -13,7 +13,6 @@ class Server:
         self.socket.bind(addr)
         self.sessions: List[User] = []
         self.requests: List[Request] = []
-        self.buffer = ''
 
     def get_user(self, addr: str, token: str) -> Optional[User]:
         """
@@ -25,30 +24,13 @@ class Server:
         return None
 
     def handle(self, data: str, addr: str) -> Mapping:
-        if self.buffer:
-            data += self.buffer
-            self.buffer = ''
-        if not data.endswith('}'):
-            *data, self.buffer = data.split('}{')
-            data = '}{'.join(data)
-            if data:
-                if self.buffer:
-                    self.buffer = '{' + self.buffer
-                data += '}'
-        if '}{' in data:
-            data = f"[{data.replace('}{', '},{')}]"
         if not data:
-            if not self.buffer:
-                return {'status': False, 'return': 'No Data given'}
-            else:
-                return None
+            return {'status': False, 'return': 'No Data was given'}
         try:
             request: Mapping = json.loads(data)
         except json.decoder.JSONDecodeError:
-            print(data)
             return {'status': False, 'return': 'No JSON could be parsed'}
-        print(addr, request)
-        return {}
+        return {'status': True}
 
     def main(self):
         logging.info(f'Server started at {self.addr}')
@@ -65,5 +47,5 @@ class Server:
             if response is None:
                 continue
             response = json.dumps(response)
-            self.socket.sendto(bytes(response, 'ascii'), addr)
+            self.socket.sendto(response.encode(), addr)
     
