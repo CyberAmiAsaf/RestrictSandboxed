f44b7f8d1e715701f39731388e5e660226e93052
Dekel Yigal <dekelyi@gmail.com>
2018-05-29 20:51:00 +0300

    First stage WIP Server

---
+++ restricted/server/user.py
@@ -0,0 +1,9 @@
+from restricted import User as _User
+from restricted.utils import random_str
+
+
+class User(_User):
+    def __init__(self, addr: str, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.addr = addr
+        self.token = random_str(8)
