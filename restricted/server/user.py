450c7ce86aa6dce28e925e1b65d63207fad3bd44
Dekel Yigal <dekelyi@gmail.com>
2018-05-30 13:49:21 +0300

    style: fix linting

--- restricted/server/user.py
+++ restricted/server/user.py
@@ -1,8 +1,14 @@
+"""
+Restricted User
+"""
 from restricted import User as _User
 from restricted.utils import random_str
 
 
 class User(_User):
+    """
+    A resticrtable user accessed via the server
+    """
     def __init__(self, addr: str, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.addr = addr
