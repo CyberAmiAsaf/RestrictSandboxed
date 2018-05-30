f7cdd840be6a008e29dbc0792d03bc9d5ed39b05
Dekel Yigal <dekelyi@gmail.com>
2018-05-30 16:51:51 +0300

    feat(server): add `set_fs_file_premission` command

---
+++ restricted/server/commands/set_fs_file_premission.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+from typing import Optional, Dict
+from ..user import User
+from ..server import Server
+
+
+def main(server: Server, addr: str, token: str, path: str, mode: Optional[str] = None) -> Dict:
+    user = server.get_user(addr, token)
+    if user is None:
+        return {'status': False, 'return': 'No user found'}
+    kwargs = {}
+    if mode:
+        kwargs['mode'] = mode
+    user.set_fs_file_premission(Path(path), **kwargs)
+    return {'status': True}
+    
