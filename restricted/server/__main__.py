9b974e8b941b222739ee8f6d6b69b5eb94bf502a
Dekel Yigal <dekelyi@gmail.com>
2018-06-22 17:58:27 +0300

    fix(server): check sudo before start
    fixes #12

--- restricted/server/__main__.py
+++ restricted/server/__main__.py
@@ -11,11 +11,22 @@ ADDR = '/tmp/restricted.socket'
 PID_FILE = '/tmp/restricted.pid'
 
 
+def check_sudo():
+    """
+    Exit the program if the process has no root premissions
+
+    :raise SystemExit: if the user uid is not 0
+    """
+    if os.getuid() != 0:
+        sys.exit('Error: must be run as root (uid 0)')
+
+
 def main():
     """
     Main server launcher
     """
     logging.getLogger().setLevel(logging.INFO)
+    check_sudo()
     with open(PID_FILE, 'w+') as fd:
         try:
             fcntl.flock(fd, fcntl.LOCK_EX | fcntl.LOCK_NB)
@@ -24,6 +35,8 @@ def main():
             sys.exit(2)
         try:
             Server(ADDR).main()
+        except KeyboardInterrupt:
+            pass
         finally:
             fcntl.flock(fd, fcntl.LOCK_UN)
             os.unlink(ADDR)
